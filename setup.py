e181033263f9ea6144daff6293d4f91b9411b018
C. E. Ball <ceball@gmail.com>
2018-02-14 21:09:06 +0000

    Untested conda package.

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup
 
 import versioneer
 
-setup(
+setup_args = dict(
     name='sphinx_ioam_theme',
-    version=versioneer.get_version(),    
+    version=versioneer.get_version(),
+    url="https://github.com/ioam/sphinx_ioam_theme",
+    description="For nbsite",
+    license="BSD-3",
     zip_safe=False,
     packages=['sphinx_ioam_theme'],
     package_data={'sphinx_ioam_theme': [
@@ -21,4 +24,12 @@ setup(
             'sphinx_ioam_theme = sphinx_ioam_theme',
         ]
     },
+    python_requires = ">=2.7",
+
+    install_requires =[
+        "sphinx"
+    ]
 )
+
+if __name__=="__main__":
+    setup(**setup_args)
