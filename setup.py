fd31d15d305d943e74ad710c2bae311a457c316a
Julia Signell <jsignell@gmail.com>
2019-05-10 14:57:26 -0400

    Trying to get release working (#8)
    * Fixing version
    
    * Fixing up indentation
    
    * Last fix to init

--- setup.py
+++ setup.py
@@ -1,41 +1,16 @@
 from setuptools import setup
 
-NAME = 'sphinx_pyviz_theme'
-DESCRIPTION = "PyViz theme for use with nbsite"
-
-# duplicated from pyct.build.get_setup_version until pyct[build] >0.4.6 lands
-def get_setup_version(root, reponame):
-    """
-    Helper to get the current version from either git describe or the
-    .version file (if available) - allows for param to not be available.
-    Normally used in setup.py as follows:
-    >>> from pyct.build import get_setup_version
-    >>> version = get_setup_version(__file__, reponame)  # noqa
-    """
-    import os
-    import json
-
-    filepath = os.path.abspath(os.path.dirname(root))
-    version_file_path = os.path.join(filepath, reponame, '.version')
-    try:
-        from param import version
-    except:
-        version = None
-    if version is not None:
-        return version.Version.setup_version(filepath, reponame, archive_commit="$Format:%h$")
-    else:
-        print("WARNING: param>=1.6.0 unavailable. If you are installing a package, this warning can safely be ignored. If you are creating a package or otherwise operating in a git repository, you should install param>=1.6.0.")
-        return json.load(open(version_file_path, 'r'))['version_string']
+import versioneer
 
 setup_args = dict(
-    name=NAME,
-    version=get_setup_version(__file__, NAME),
+    name='sphinx_pyviz_theme',
+    version=versioneer.get_version(),
     url="https://github.com/pyviz-dev/sphinx_pyviz_theme",
-    description=DESCRIPTION,
+    description="For nbsite",
     license="BSD-3",
     zip_safe=False,
-    packages=[NAME],
-    package_data={NAME: [
+    packages=['sphinx_pyviz_theme'],
+    package_data={'sphinx_pyviz_theme': [
         'theme.conf',
         '*.html',
         'includes/*.html',
@@ -51,16 +26,9 @@ setup_args = dict(
     },
     python_requires = ">=2.7",
 
-    install_requires = [
-        "sphinx",
-        "param >=1.7.0",
-    ],
-    extras_require = {
-        'build': [
-            "param >=1.7.0",
-            "setuptools",
-        ]
-    }
+    install_requires =[
+        "sphinx"
+    ]
 )
 
 if __name__=="__main__":
--- sphinx_pyviz_theme/__init__.py
+++ sphinx_pyviz_theme/__init__.py
@@ -1,12 +1,7 @@
-import os.path
-import param
-
-NAME = "sphinx_pyviz_theme"
-
-# version comes from git if available, otherwise from .version file
-__version__ = str(param.version.Version(fpath=__file__, archive_commit="$Format:%h$",
-                                        reponame=NAME))
+import os
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
 
 def setup(app):
-    app.add_html_theme('sphinx_pyviz_theme', os.path.abspath(os.path.dirname(__file__)))
-
+   app.add_html_theme('sphinx_pyviz_theme', os.path.abspath(os.path.dirname(__file__)))
